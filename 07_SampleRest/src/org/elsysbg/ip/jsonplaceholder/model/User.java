e1a389077527ddb777bcadcd8f0b2794038a0c62
denislav-7128 <den4o_597@abv.bg>
2015-04-01 13:54:29 +0300

    Password is not send to the client

--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/model/User.java
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/model/User.java
@@ -1,17 +1,14 @@
 package org.elsysbg.ip.jsonplaceholder.model;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  * Model is copied by http://jsonplaceholder.typicode.com/
@@ -31,11 +28,9 @@ public class User {
 	@Column(nullable=false, length=50, unique=true)
 	private String email;
 	
-	@OneToMany(mappedBy="author",
-		fetch=FetchType.EAGER)
-    @XmlElement
-	private List<Post> posts;
-
+	// posts of the user will be get with separate REST call
+	// (e.g. no need of posts of the user when searching for user)
+	
 	// TODO password should be stored in DB as hash+salt
 	// TODO read about storing passwords, hashing, salting, etc.
 	@Column(nullable=false, length=50)
@@ -52,9 +47,17 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	@XmlTransient
+	// Password is a secret and
+	// must not be send to the client
 	public String getPassword() {
 		return password;
 	}
+	
+	@XmlElement
+	// User should be able to set its own password
+	// XmlElement is required because of XmlTransient in the getter 
 	public void setPassword(String password) {
 		this.password = password;
 	}
